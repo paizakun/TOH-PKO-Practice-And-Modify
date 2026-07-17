3039c50f960c30e3d9058e5b18cd00e4c1a1426a
Ereshkigal <118900266+paizakun@users.noreply.github.com>
2026-07-18 03:34:41 +0900

    Phantomボタンに合わせた仕様に変更

--- Roles/Impostor/KillCoolSyncTest.cs
+++ Roles/Impostor/KillCoolSyncTest.cs
@@ -34,5 +34,14 @@ public sealed class KillCoolSyncTest : RoleBase, IImpostor, IUsePhantomButton
         => OptionSyncValue = FloatOptionItem.Create(RoleInfo, 10, OptionName.KillCoolSyncTestSyncValue, new(0f, 180f, 0.5f), 1f, false).SetValueFormat(OptionFormat.Seconds);
 
     void IUsePhantomButton.OnClick(ref bool AdjustKillCooldown, ref bool? ResetCooldown)
-        => Player.SyncKillCooldown(OptionSyncValue.GetFloat());
+    {
+        // AdjustKillCooldownをtrueのままにすると、この後ActionButtonPatch側が
+        // 独自計算した値でMain.AllPlayerKillCooldownを上書きしてしまうため、falseにして防ぐ。
+        // ただしfalseにするとActionButtonPatch側のIPPlayerKillCooldownリセットもスキップされる
+        // (前回クリックからの経過時間が残り続け、2回目以降のクールが正しく計算されない)ため、
+        // Eater/DoubleKillerなど自前管理の役職と同様に自分でリセットする
+        AdjustKillCooldown = false;
+        Player.SyncKillCooldown(OptionSyncValue.GetFloat());
+        IUsePhantomButton.IPPlayerKillCooldown[Player.PlayerId] = 0f;
+    }
 }
